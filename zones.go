f5c70a1e5dbc8d543cce4dcd0e3613bb3bb60a99
ZoneDBot <robot@zonedb.org>
2017-06-04 00:39:38 +0000

    autopull: 2017-06-04T00:30:28Z

--- zones.go
+++ zones.go
@@ -3952,7 +3952,7 @@ var _y = [4358]Zone{
 	{"be.tc", &_z[1362], nil, _c[0:6], nil, NS{"ns1.above.com", "ns2.above.com", "ns.udag.de", "ns.udagdns.de", "ns1.parkingcrew.net", "ns2.parkingcrew.net", "ns.udag.net", "ns.udagdns.net", "ns.udag.org"}, nil, "", "", "", 0x0},
 	{"bg.tc", &_z[1362], nil, _c[0:6], nil, NS{"ns1.4.cn", "ns2.4.cn", "f1g1ns1.dnspod.net", "f1g1ns2.dnspod.net"}, nil, "", "", "", 0x0},
 	{"biz.tc", &_z[1362], nil, _c[0:6], nil, NS{"ns1.codrv.net", "ns3.codrv.net", "dns1.freehostservers.net", "dns2.freehostservers.net"}, nil, "", "", "http://www.biz.tc/", 0x0},
-	{"br.tc", &_z[1362], nil, _c[0:6], nil, NS{"ns1.above.com", "ns2.above.com", "ns1.parkingcrew.net", "ns2.parkingcrew.net"}, nil, "", "", "", 0x0},
+	{"br.tc", &_z[1362], nil, _c[0:6], nil, NS{"ns1.above.com", "ns2.above.com", "ns1.parkingcrew.net", "ns2.parkingcrew.net", "default1-tr.nic.tc", "default2-tr.nic.tc"}, nil, "", "", "", 0x0},
 	{"ch.tc", &_z[1362], nil, _c[0:6], nil, NS{"ns1.alidns.com", "ns2.alidns.com", "v1s1.xundns.com", "v1s2.xundns.com", "f1g1ns2.dndpod.net", "f1g1ns1.dnspod.net", "suspended.nic.tc"}, nil, "", "", "", 0x0},
 	{"com.tc", &_z[1362], nil, nil, nil, NS{"root1.zone.tc", "root2.zone.tc", "root3.zone.tc", "root4.zone.tc", "root5.zone.tc", "root6.zone.tc", "root7.zone.tc", "root8.zone.tc"}, nil, "", "", "", 0x0},
 	{"cz.tc", &_z[1362], nil, _c[0:6], nil, NS{"f1g1ns1.dnspod.net", "f1g1ns2.dnspod.net", "default1.nic.tc", "default2.nic.tc", "suspended.nic.tc"}, nil, "", "", "", 0x0},
