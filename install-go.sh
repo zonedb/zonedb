2baaa7861231cdb93b3dea82b63ff5a3d2ce9dee
Randy Reddig <ydnar@shaderlab.com>
2018-02-09 09:14:23 -0800

    revert last commit

--- install-go.sh
+++ install-go.sh
@@ -5,5 +5,6 @@ set -u
 
 curl -o /app/go.tgz https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz
 tar xvf /app/go.tgz -C /app
-export GOPATH=/app/go PATH=/app/go/bin:$PATH
+mkdir /app/go2
+export GOROOT=/app/go GOPATH=/app/go2 PATH=/app/go/bin:$PATH
 go get -t -d -v ./...
