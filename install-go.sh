5428edb11bf2cda778b3803e80ccb87ab74fe9f9
Randy Reddig <ydnar@shaderlab.com>
2018-02-09 09:16:53 -0800

    mkdir -p

--- install-go.sh
+++ install-go.sh
@@ -5,6 +5,6 @@ set -u
 
 curl -o /app/go.tgz https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz
 tar xvf /app/go.tgz -C /app
-mkdir /app/go2
+mkdir -p /app/go2
 export GOROOT=/app/go GOPATH=/app/go2 PATH=/app/go/bin:$PATH
 go get -t -d -v ./...
