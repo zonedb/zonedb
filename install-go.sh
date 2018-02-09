3dfa2c01140d7fdd95237772de32c2153f49f0df
Randy Reddig <ydnar@shaderlab.com>
2018-02-09 08:50:43 -0800

    Revert "Remove install-go.sh file"
    This reverts commit 7347677c08b2bb6809b82838c22e4440546df191.

---
+++ install-go.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+curl -o /app/go.tgz https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+tar xvf /app/go.tgz -C /app
+mkdir /app/go2
+export GOROOT=/app/go GOPATH=/app/go2 PATH=/app/go/bin:$PATH
+go get -t -d -v ./...
