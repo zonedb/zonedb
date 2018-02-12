ef5a58dc4ee32234f9cda300e0feb92f29b39afb
Randy Reddig <ydnar@shaderlab.com>
2018-02-11 18:22:28 -0800

    internal/build: back to TCP DNS

--- internal/build/dns.go
+++ internal/build/dns.go
@@ -330,7 +330,7 @@ func exchange(ctx context.Context, host, qname string, qtype uint16) (*dns.Msg, 
 	qmsg := &dns.Msg{}
 	qmsg.MsgHdr.RecursionDesired = false
 	qmsg.SetQuestion(dns.Fqdn(qname), qtype)
-	client := &dns.Client{}
+	client := &dns.Client{Net: "tcp"}
 	rmsg, _, err := client.ExchangeContext(ctx, qmsg, host+":53")
 	return rmsg, err
 }
