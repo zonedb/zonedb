95bbf983b83b9feede8b63fae6bd1bb411c5f0a6
Randy Reddig <randy@domainr.com>
2021-04-04 09:57:44 -0700

    cmd/zonedb: rename -info-url flag to -set-info-url

--- cmd/zonedb/main.go
+++ cmd/zonedb/main.go
@@ -39,7 +39,7 @@ func main() {
 	checkPS := flag.Bool("ps", false, "check against Public Suffix List")
 
 	// Mutate operations
-	infoURL := flag.String("info-url", "", "set zone(s) info URL")
+	setInfoURL := flag.String("set-info-url", "", "set zone(s) info URL")
 	addTags := flag.String("add-tags", "", "add tags to zones (comma-delimited)")
 	addLocations := flag.String("add-locations", "", "add locations to zones (comma-delimited)")
 	removeTags := flag.String("remove-tags", "", "remove tags from zones (comma-delimited)")
@@ -273,11 +273,11 @@ func main() {
 		color.Fprintf(os.Stderr, "@{.}Zones: @{c}%s\n", strings.Join(zones.Values(), " "))
 	}
 
-	if *infoURL != "" {
+	if *setInfoURL != "" {
 		for _, z := range workZones {
-			z.InfoURL = *infoURL
+			z.InfoURL = *setInfoURL
 		}
-		color.Fprintf(os.Stderr, "@{.}Set info URL to: @{c}%s\n", *infoURL)
+		color.Fprintf(os.Stderr, "@{.}Set info URL to: @{c}%s\n", *setInfoURL)
 	}
 
 	if *verifyNS {
