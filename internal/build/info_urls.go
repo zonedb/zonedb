3ce5811c960a08e8f2f6d08700ecc7938b860b96
Randy Reddig <randy@domainr.com>
2021-04-04 10:35:34 -0700

    internal/build: use custom http.Transport and close response body

--- internal/build/info_urls.go
+++ internal/build/info_urls.go
@@ -1,6 +1,8 @@
 package build
 
 import (
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
@@ -12,8 +14,12 @@ import (
 func UpdateInfoURLs(zones map[string]*Zone) {
 	color.Fprintf(os.Stderr, "@{.}Updating info URLs for %d zones...\n", len(zones))
 
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	transport.TLSHandshakeTimeout = 2 * time.Second
+	transport.MaxIdleConnsPerHost = 10
 	client := &http.Client{
-		Timeout: 2 * time.Second,
+		Transport: transport,
+		Timeout:   4 * time.Second,
 	}
 
 	mapZones(zones, func(z *Zone) {
@@ -40,7 +46,20 @@ func UpdateInfoURLs(zones map[string]*Zone) {
 				z.InfoURL = ru
 				color.Fprintf(os.Stderr, "@{.}Updated info URLs for @{c}%s@{c}: @{y}%s\n", z.Domain, z.InfoURL)
 			}
+			CloseN(res.Body, 10_000_000)
 			break
 		}
 	})
+}
+
+// CloseN drains rc up to a maximum of n bytes and closes rc.
+// It returns the number of bytes drained and the first error encountered.
+// Regardless of any errors, rc.Close() is guaranteed to be called.
+func CloseN(rc io.ReadCloser, n int64) (int64, error) {
+	n, cerr := io.Copy(ioutil.Discard, io.LimitReader(rc, n))
+	err := rc.Close()
+	if cerr != nil {
+		err = cerr
+	}
+	return n, err
 }
